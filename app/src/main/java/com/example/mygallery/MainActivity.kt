18302a32d4f1a35fff155a1e92505c566e44210c
Namikng <namikng@gmail.com>
2019-12-11 01:35:21 +0900

    프로젝트 업로드

---
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.mygallery
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 권한이 부여되었는지 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 부여되지 않았을때
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // 이전에 이미 사용자가 권한 요청을 거부한 경우
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다", "권한이 필요한 이유") {
+                    yesButton {
+                        // 권한 요청
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_STORAGE)
+                    }
+                    noButton {  }
+                }.show()
+            } else {
+                // 권한 요청
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            // 권한이 이미 허용됨
+            getAllPhotos()
+        }
+    }
+
+    private fun getAllPhotos() {
+        // 모든 사진 정보 가져오기
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null, // 가져올 항목 배열
+            null, // 조건
+            null, // 조건
+            MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC") // 찍은 날짜 내림차순
+    }
+}
